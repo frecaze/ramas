a2c04de8eeca9c8533a28ea5051b7ba343edd6f6
ALUMNO09-J1101 <alumno09-j1101@TLSCPU0717.local>
2018-06-20 17:34:41 -0500

    ejercicio swift

---
+++ ramas.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func estadocivil(numerocivil: Int) -> String {
+    var estado = ""
+    switch numerocivil {
+    case 1:
+        estado = "soltero"
+        break
+    case 2:
+        estado = "casado"
+        break
+    case 3:
+        estado = "viudo"
+        break
+    case 4:
+        estado = "divorsiado"
+        break
+    default:
+      estado = "no esta dentro del rango"
+    }
+    
+    return estado
+}
+estadocivil(numerocivil: 1)
