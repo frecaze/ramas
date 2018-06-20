dacb18424fb9b6adc0895f1efde077b8d4d51c92
ALUMNO10-J1101 <alumno10-j1101@TLSCPU0726.local>
2018-06-20 18:02:15 -0500

    agrego ejercicios

---
+++ ejercicioPractica.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+func calcularestado(categoria:String) -> Double {
+    var estado:Double = 0
+    
+    if categoria == "Soltero"{
+        estado = 1
+    }
+    if categoria == "Casado"{
+        estado = 2
+    }
+    if categoria == "Viudo"{
+        estado = 3
+    }
+    if categoria == "Divorciado"{
+        estado = 4
+    }
+    return estado
+
+}
+calcularestado(categoria:"Casado")*/
+
+/*func estadocivil(numerocivil: Int) -> String {
+    var estado = ""
+    switch numerocivil {
+    case 1:
+        estado = "soltero"
+        break
+    case 2:
+        estado = "casado"
+        break
+    case 3:
+        estado = "viudo"
+        break
+    case 4:
+        estado = "divorsiado"
+        break
+    default:
+        estado = "no existe"
+    }
+    
+    return estado
+}
+estadocivil(numerocivil: 1)*/
+
+/*func calcular(cuadro:Int) -> Int{
+    var total = cuadro * cuadro
+    return total
+}
+calcular(cuadro:10)*/
+
+var calcular = {
+    (cuadro:Int) -> Int in
+    var total = cuadro * cuadro
+    return total
+}
+calcular(10)
+
