dbde1faaeb33d9d30742efe9c9f7d8771a0ff0de
ALUMNO10-J1101 <alumno10-j1101@TLSCPU0726.local>
2018-06-21 17:53:24 -0500

    agrego cambios final

--- ejercicioPractica.playground/Contents.swift
+++ ejercicioPractica.playground/Contents.swift
@@ -52,7 +52,7 @@ estadocivil(numerocivil: 1)*/
 }
 calcular(cuadro:10)*/
 
-var calcular = {
+/*var calcular = {
     (cuadro:Int) -> Int in
     var total = cuadro * cuadro
     return total
@@ -80,4 +80,55 @@ var estadocivil = {
     }
     return estado
 }
-estadocivil(1)
+estadocivil(1)*/
+
+/*class celular {
+
+    var color = [""]
+
+    var precio = 0.0
+
+    var peso = 0.0
+}
+
+var s6 = celular()
+
+s6.color
+s6.precio
+s6.peso
+
+s6.color = ["azul", "verde", "rojo"]
+
+s6.color[1]*/
+
+
+class persona {
+    var nombre = "Joel"
+    var apellido = "Davila Sto"
+    var edad = 28
+    var sexo = "Hombre"
+    var dni = 76030150
+    
+    func nombreCompleto() -> String{
+        var nombrecm = nombre + " " + apellido
+        return nombrecm
+    }
+    
+    func edadActual() -> Int{
+        var calcedad = edad
+        return calcedad
+    }
+}
+
+var p = persona()
+
+p.nombreCompleto()
+
+p.edadActual()
+
+
+
+
+
+
+
